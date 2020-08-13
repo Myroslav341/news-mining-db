2d8e3afb5056821fa1e69cf3570d001b23065a32
Myroslav341 <gurmann341@gmail.com>
2020-08-13 23:38:16 +0300

    alembic configured, scripts add

--- setup.py
+++ setup.py
@@ -7,11 +7,26 @@ setup(
     author='Myroslav Havryliuk',
     author_email='myroslav04@gmail.com',
     # Needed to actually package something
-    packages=['news_mining_db'],
+    packages=[
+        'news_mining_db',
+        'news_mining_db.migrations',
+        'news_mining_db.models',
+        'news_mining_db.alembic',
+        'news_mining_db.commands',
+    ],
+    package_data={
+        'news_mining_db': ['alembic.ini'],
+    },
+    entry_points={
+        'console_scripts': [
+            'init-news-db=news_mining_db.commands:init_db',
+            'upgrade-db=news_mining_db.commands:upgrade_db'
+        ]
+    },
     # Needed for dependencies
     install_requires=['sqlalchemy', 'alembic', 'psycopg2'],
     # *strongly* suggested for sharing
-    version='0.3.4',
+    version='0.4',
     # The license can be anything you like
     license='MIT',
     description='Sheared db models for news-mining project',
